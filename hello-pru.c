4bf9615123d35985e455b3a5a7653dc064fd9a42
Thomas Barnette <tvbarnette999@gmail.com>
2018-04-12 03:30:21 +0000

    Initial Commit of PRU test

---
+++ hello-pru.c
@@ -0,0 +1,112 @@
+/*
+ * Source Modified by Zubeen Tolani < ZeekHuge - zeekhuge@gmail.com >
+ * Based on the examples distributed by TI
+ *
+ * Copyright (C) 2015 Texas Instruments Incorporated - http://www.ti.com/
+ *
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *	* Redistributions of source code must retain the above copyright
+ *	  notice, this list of conditions and the following disclaimer.
+ *
+ *	* Redistributions in binary form must reproduce the above copyright
+ *	  notice, this list of conditions and the following disclaimer in the
+ *	  documentation and/or other materials provided with the
+ *	  distribution.
+ *
+ *	* Neither the name of Texas Instruments Incorporated nor the names of
+ *	  its contributors may be used to endorse or promote products derived
+ *	  from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+				  
+#include <stdint.h>
+#include <pru_cfg.h>
+#include <pru_ctrl.h>
+#include "resource_table_empty.h"
+
+#define	INS_PER_US 200           // 5ns per instruction
+#define INS_PER_DELAY_LOOP 2	 // two instructions per delay loop
+#define DELAY_CYCLES_US (INS_PER_US / INS_PER_DELAY_LOOP)
+
+#define GPIO1 0x4804C000
+#define GPIO_CLEARDATAOUT 0x190
+#define GPIO_SETDATAOUT 0x194
+#define USR0 (1<<21)
+#define USR1 (1<<22)
+#define USR2 (1<<23)
+#define USR3 (1<<24)
+unsigned int volatile * const GPIO1_CLEAR = (unsigned int *) (GPIO1 + GPIO_CLEARDATAOUT);
+unsigned int volatile * const GPIO1_SET = (unsigned int *) (GPIO1 + GPIO_SETDATAOUT);
+
+volatile register unsigned int __R30;
+volatile register unsigned int __R31;
+#define PRU0_GPIO (1<<2)
+
+#ifndef DECAY_RATE
+#define DECAY_RATE 100
+#endif
+#ifndef DELAY_CYCLES
+#define DELAY_CYCLES DELAY_CYCLES_US
+#endif
+
+//#define LED_DISABLE 1
+
+const int decay = DECAY_RATE;
+
+void main(void) {
+	
+	int i, j;
+
+	/* Clear SYSCFG[STANDBY_INIT] to enable OCP master port */
+	
+	/*
+	 unsigned int loops, delay;
+    for (loops = 0; loops < 10; loops++) {
+        __R30 = __R30 | (1 << 15); // Turn on the LED
+        __R30 = __R30 | 1; 
+        for (delay = 0; delay < 20000000; delay++) { // loop delay
+        }
+        __R30 = __R30 & ~(1 << 15); // Turn off the LED
+        __R30 - __R30 & ~1;
+        __delay_cycles(60000000); // Intrinsic method delay
+    }
+	*/
+	
+	
+	CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
+
+	for(i = 1000000; i > 0; i = (i * decay) / 100) {
+#ifndef LED_DISABLE
+		*GPIO1_SET = USR0;
+#endif
+#ifndef GPIO_DISABLE
+		__R30 |= PRU0_GPIO;
+#endif
+		for(j=0;j<i;j++){ __delay_cycles(2 * DELAY_CYCLES); }
+#ifndef LED_DISABLE
+		*GPIO1_CLEAR = USR0;
+#endif
+#ifndef GPIO_DISABLE
+		__R30 ^= PRU0_GPIO;
+#endif
+		for(j=0;j<i;j++){ __delay_cycles(DELAY_CYCLES); }
+	}
+
+
+	__halt();
+}
